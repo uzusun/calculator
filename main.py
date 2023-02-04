091f9c25c555392116a7c3411f744363fa1f2e1c
uzusun <ektkfdl@naver.com>
2023-02-04 09:03:22 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox,QPlainTextEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox,QPlainTextEdit, QHBoxLayout) # QHBoxLayout 추가
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -15,11 +15,19 @@ class Calculator(QWidget):
         
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭시 핸들러 함수 연결
+        self.btn2=QPushButton('Clear',self) # 버튼 2 추가
+        self.btn2.clicked.connect(self.clearMessage) # 버튼 2 핸들러 함수 연결
+
+        hbox = QHBoxLayout()    # 수평 박스 레이아웃을 추가하고 버튼1, 2 추가
+        hbox.addStretch(1)  # 공백
+        hbox.addWidget(self.btn1) # 버튼 1 배치
+        hbox.addWidget(self.btn2) # 버튼 2 배치
 
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addStretch(1)
-        vbox.addWidget(self.btn1)
+        #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -29,6 +37,9 @@ class Calculator(QWidget):
         self.resize(256,256)
         self.show()
 
+    def clearMessage(self): # 버튼 2 핸들러 함수 정의
+        self.te1.clear()
+        
     def activateMessage(self):
         #QMessageBox.information(self,"information","Button clicked!")
         self.te1.appendPlainText("Button clicked!")
