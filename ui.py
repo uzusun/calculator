ad7b9314da66f512f42fa82ed9d59b691e9a41fb
uzusun <ektkfdl@naver.com>
2023-02-05 12:26:35 +0900

    Add lbl1 in ui.py to print current date

---
+++ ui.py
@@ -0,0 +1,43 @@
+# ch 5.2.1 ui.py
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,QMessageBox,QPlainTextEdit,QHBoxLayout,QLabel)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QDate, Qt  # 날짜와 주요 속성값 사용을 위해 추가
+
+class View(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.date = QDate.currentDate() # 현재 날짜를 저장하기 위해 추가
+        self.initUI()
+
+    def initUI(self):
+        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)  # 추가
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
+        self.btn1 = QPushButton('Message',self)
+        self.btn2 = QPushButton('Clear',self)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
+        vbox.addLayout(hbox)
+        vbox.addWidget(self.lbl1)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
+        self.show()
+
+    def activateMessage(self):
+        self.te1.appendPlainText("Button clicked!")
+
+    def clearMessage(self):
+        self.te1.clear()
